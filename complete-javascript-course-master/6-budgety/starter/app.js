329e4c052d282ab385ba19a8225308b6ea1f8724
Anders Ulven <anders.ulven@telenor.com>
2020-10-08 12:48:42 +0200

    init

---
+++ complete-javascript-course-master/6-budgety/starter/app.js
@@ -0,0 +1,77 @@
+// BUDGET CONTROLLER
+var budgetController = (function(){
+    // Some code
+})();
+
+// UI CONTROLLER
+var UIController = (function(){
+    
+    var DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
+    };
+
+    return {
+        getInput: function(){
+            return {
+                type: document.querySelector(DOMstrings.inputType).value, // Will be either inc(income) or exp(expences)
+                description: document.querySelector(DOMstrings.inputDescription).value, // 
+                value: document.querySelector(DOMstrings.inputValue).value, // 
+            };
+        },
+        
+        getDOMstrings: function(){
+            return DOMstrings;
+        }
+    };
+
+})();
+
+
+
+
+
+// GLOBAL APP CONTROLLER
+var controller = (function(budgetCtrl, UICtrl){
+    
+    var setupEventListeners = function() {
+        var DOM = UICtrl.getDOMstrings();
+
+        document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+
+        document.addEventListener('keypress', function(event){
+            if (event.keyCode === 13 && event.which === 13) {
+                ctrlAddItem();
+            }
+        });
+    };
+
+    var ctrlAddItem = function() {
+        // 1. Get the field input data
+        var input = UICtrl.getInput();
+
+        // 2. Add the item to the budget controller
+
+        // 3. Add the item to the UI
+
+        // 4. Clear the fields
+
+        // 5. Calculate and update budget
+        
+        // 6. Calculate and update percentages        
+    };
+    
+    return {
+        init: function() {
+            console.log('Application has started.');
+            setupEventListeners();
+        }
+    };
+
+
+
+})();
+
+controller.init();
